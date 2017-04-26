5e28368703ac7db72f7157df114029a24a0b3aaf
John <johnitolee@gmail.com>
2017-04-25 23:37:41 -0700

    Add text flipper

--- javascript/textflip.js
+++ javascript/textflip.js
@@ -1,32 +1,32 @@
 (function() {
 
   let wordArray = ["Hello World", "Welcome"];
-  let element = document.getElementsByClassName("text")[0];
+  let elementText = document.getElementsByClassName("text")[0];
   let index = 1;
 
   let resetAnimation = function (){
-    element.classList.remove("flip");
+    elementText.classList.remove("flip");
   };
 
   setInterval(function(){
     switch(index){
       case 0:
-        element.classList.add('flip');
-        element.textContent = wordArray[index];
+        elementText.classList.add('flip');
+        elementText.textContent = wordArray[index];
         index = 1;
         setTimeout(resetAnimation, 1000);
       break;
 
       case 1:
-        element.classList.add('flip');
-        element.textContent = wordArray[index];
+        elementText.classList.add('flip');
+        elementText.textContent = wordArray[index];
         index = 0;
         setTimeout(resetAnimation, 1000);
       break;
 
       // case 2:
-      //   element.classList.add('flip');
-      //   element.textContent = wordArray[index];
+      //   elementText.classList.add('flip');
+      //   elementText.textContent = wordArray[index];
       //   index = 0;
       //   setTimeout(resetAnimation, 1000);
       // break;
